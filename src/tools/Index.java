ac5302e8c2206f9cfc54d7a67f73df92dea7380a
Chess erindale <chess.erindale@gmail.com>
2023-04-26 23:15:40 -0400

    Deleted AI methods from Player.java

--- src/tools/Index.java
+++ src/tools/Index.java
@@ -25,8 +25,8 @@ package tools;
 * ----------------------------------------------------------------------------------
 * Date			Person	CSR#	Description
 * 2023-04-19	SA 		1.01	Initial Version 
-* 2023-04-20	SA 		1.01	Add comments to methods
-* 2023-04-20	SA 		1.01	Deleted unnecessary or not understood methods 
+* 2023-04-20	SA 		1.02	Add comments to methods
+* 2023-04-20	SA 		1.03	Deleted unnecessary or not understood methods 
 * ----------------------------------------------------------------------------------
 * Future Enhancements: 
 * -------------------- 	
